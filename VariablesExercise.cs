c56069ed9d3eb25d2dd8d5567958734355784317
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-21 15:31:40 -0500

    variables excercise

---
+++ VariablesExercise.cs
@@ -0,0 +1,8 @@
+string fishName = "Bubbles";
+int fishAge = 1;
+char fishMiddleName = 'P';
+bool fishFreindly = false;
+double fishWeight = 1.5;
+decimal fishHeight = 0.55m;
+
+System.Console.WriteLine();
